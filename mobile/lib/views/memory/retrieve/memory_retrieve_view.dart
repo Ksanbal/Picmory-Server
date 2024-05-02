bca8dbcacb6ca8aa8a16ca5b75c84d2667af706d
Ksanbal <dev.ksanbal@gmail.com>
2024-05-02 20:41:07 +0900

    Feat: 상세보기 UI 변경

--- mobile/lib/views/memory/retrieve/memory_retrieve_view.dart
+++ mobile/lib/views/memory/retrieve/memory_retrieve_view.dart
@@ -1,7 +1,8 @@
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
-import 'package:picmory/common/families/color_family.dart';
+import 'package:picmory/common/components/memory/retrieve/video_player.dart';
 import 'package:picmory/common/families/text_styles/title_sm_style.dart';
 import 'package:picmory/viewmodels/memory/retrieve/memory_retrieve_viewmodel.dart';
 import 'package:provider/provider.dart';
@@ -17,134 +18,153 @@ class MemoryRetrieveView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final vm = Provider.of<MemoryRetrieveViewmodel>(context, listen: true);
+    final vm = Provider.of<MemoryRetrieveViewmodel>(context, listen: false);
     vm.getMemory(int.parse(memoryId));
 
     return Scaffold(
-      backgroundColor: vm.isFullScreen ? Colors.black : ColorFamily.disabledGrey400,
+      backgroundColor: Colors.black,
       body: Stack(
+        alignment: Alignment.center,
         children: [
-          Align(
-            alignment: Alignment.center,
-            child: vm.memory == null
-                ? Container()
-                : InkWell(
-                    onTap: vm.toggleFullScreen,
-                    child: PageView.builder(
-                      itemCount: vm.photos.length,
-                      itemBuilder: (_, index) {
-                        final photo = vm.photos[index];
+          // 이미지
+          Consumer<MemoryRetrieveViewmodel>(
+            builder: (_, vm, __) {
+              if (vm.memory == null) return Container();
 
-                        return ExtendedImage.network(
-                          photo.uri,
-                          mode: ExtendedImageMode.gesture,
-                        );
-                      },
+              return PageView.builder(
+                itemCount: vm.photos.length + vm.videos.length,
+                itemBuilder: (_, index) {
+                  if (index < vm.photos.length) {
+                    final photo = vm.photos[index];
+
+                    return ExtendedImage.network(
+                      photo.uri,
+                      mode: ExtendedImageMode.gesture,
+                    );
+                  } else {
+                    final video = vm.videos[index - vm.photos.length];
+
+                    return Padding(
+                      padding: EdgeInsets.only(
+                        top: 48 + MediaQuery.of(context).padding.top,
+                        bottom: 48 + MediaQuery.of(context).padding.bottom,
+                      ),
+                      child: VideoPlayer(
+                        fromNetwork: true,
+                        uri: video.uri,
+                        file: null,
+                      ),
+                    );
+                  }
+                },
+              );
+            },
+          ),
+          // 상단
+          Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(
+                4,
+                MediaQuery.of(context).padding.top,
+                4,
+                0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // 뒤로가기
+                  InkWell(
+                    onTap: context.pop,
+                    child: const SizedBox(
+                      width: 48,
+                      height: 48,
+                      child: Icon(
+                        SolarIconsOutline.altArrowLeft,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
+                  // 삭제
+                  InkWell(
+                    onTap: () => vm.delete(context),
+                    child: const SizedBox(
+                      width: 48,
+                      height: 48,
+                      child: Icon(
+                        SolarIconsOutline.trashBinMinimalistic,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-          // appbar
-          vm.isFullScreen
-              ? Container()
-              : Container(
-                  height: 64 + MediaQuery.of(context).padding.top,
-                  padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).padding.top,
+          // 하단
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(
+                16,
+                0,
+                4,
+                MediaQuery.of(context).padding.bottom,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // 날짜 변경
+                  InkWell(
+                    onTap: () => vm.showChangeDateBottomsheet(context),
+                    child: Text(
+                      vm.memory?.date != null
+                          ? DateFormat('yyyy.MM.dd').format(vm.memory!.date)
+                          : '',
+                      style: const TitleSmStyle(
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  child: Stack(
-                    alignment: Alignment.center,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: InkWell(
-                          onTap: () => vm.pop(context),
-                          child: const CircleAvatar(
-                            radius: 24,
-                            backgroundColor: Colors.white,
-                            child: Icon(
-                              SolarIconsOutline.altArrowLeft,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                      ),
+                      // 좋아요
                       InkWell(
-                        onTap: () => vm.showChangeDateBottomsheet(context),
-                        child: Text(
-                          vm.memory?.date != null
-                              ? DateFormat('yyyy.MM.dd').format(vm.memory!.date)
-                              : '',
-                          style: const TitleSmStyle(),
+                        onTap: vm.likeMemory,
+                        child: SizedBox(
+                          width: 48,
+                          height: 48,
+                          child: Consumer<MemoryRetrieveViewmodel>(
+                            builder: (_, vm, __) {
+                              final isLiked = vm.memory?.isLiked ?? false;
+
+                              return Icon(
+                                isLiked ? SolarIconsBold.heart : SolarIconsOutline.heart,
+                                color: Colors.white,
+                              );
+                            },
+                          ),
                         ),
                       ),
-                      if (vm.videos.isNotEmpty)
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: IconButton(
-                            onPressed: () => vm.showVideoPlayerDialog(context),
-                            icon: const Icon(Icons.video_library_outlined),
+                      // 앨범 추가
+                      InkWell(
+                        onTap: () => vm.showAddAlbumDialog(context),
+                        child: const SizedBox(
+                          width: 48,
+                          height: 48,
+                          child: Icon(
+                            SolarIconsOutline.addFolder,
+                            color: Colors.white,
                           ),
-                        )
+                        ),
+                      ),
                     ],
                   ),
-                ),
-          // 하단 선택바
-          vm.isFullScreen
-              ? Container()
-              : Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    height: 76 + MediaQuery.of(context).padding.bottom,
-                    color: Colors.white,
-                    padding: EdgeInsets.only(
-                      bottom: MediaQuery.of(context).padding.bottom,
-                    ),
-                    child: Row(
-                      children: [
-                        // 정보 토글
-                        Expanded(
-                          child: IconButton(
-                            icon: const Icon(SolarIconsOutline.infoCircle),
-                            onPressed: () {},
-                          ),
-                        ),
-                        // 좋아요
-                        Expanded(
-                          child: IconButton(
-                            icon: Consumer<MemoryRetrieveViewmodel>(
-                              builder: (_, vm, __) {
-                                final isLiked = vm.memory?.isLiked ?? false;
-
-                                return Icon(
-                                  isLiked ? SolarIconsBold.heart : SolarIconsOutline.heart,
-                                  color: isLiked ? ColorFamily.error : null,
-                                );
-                              },
-                            ),
-                            onPressed: vm.likeMemory,
-                          ),
-                        ),
-                        // 앨범 추가
-                        Expanded(
-                          child: IconButton(
-                            icon: const Icon(SolarIconsOutline.addFolder),
-                            onPressed: () => vm.showAddAlbumDialog(context),
-                          ),
-                        ),
-                        // 삭제
-                        Expanded(
-                          child: IconButton(
-                            icon: const Icon(SolarIconsOutline.trashBinMinimalistic),
-                            onPressed: () => vm.delete(context),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
