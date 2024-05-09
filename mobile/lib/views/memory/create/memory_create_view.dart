f819b3bec71d4ee935c2ea5cbaadb890c4e339d6
Ksanbal <dev.ksanbal@gmail.com>
2024-05-09 21:19:28 +0900

    Feat: 추억 등록 페이지 디자인 적용

--- mobile/lib/views/memory/create/memory_create_view.dart
+++ mobile/lib/views/memory/create/memory_create_view.dart
@@ -2,10 +2,15 @@ import 'dart:io';
 
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:picmory/common/buttons/rounded_button.dart';
 import 'package:picmory/common/components/memory/retrieve/video_player.dart';
+import 'package:picmory/common/families/color_family.dart';
+import 'package:picmory/common/families/text_styles/text_sm_style.dart';
 import 'package:picmory/viewmodels/memory/create/memory_create_viewmodel.dart';
 import 'package:provider/provider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:solar_icons/solar_icons.dart';
 
 class MemoryCreateView extends StatelessWidget {
   const MemoryCreateView({super.key});
@@ -18,15 +23,21 @@ class MemoryCreateView extends StatelessWidget {
     return Stack(
       children: [
         Scaffold(
+          extendBodyBehindAppBar: true,
           appBar: AppBar(
+            backgroundColor: ColorFamily.backgroundGrey200,
+            leading: IconButton(
+              onPressed: context.pop,
+              icon: const Icon(SolarIconsOutline.altArrowLeft),
+            ),
             actions: [
               // 영상 추가 버튼
               Consumer<MemoryCreateViewmodel>(
                 builder: (_, vm, __) {
                   if (vm.galleryVideos.isEmpty) {
-                    return TextButton(
+                    return IconButton(
                       onPressed: vm.selectVideo,
-                      child: const Text("영상 추가"),
+                      icon: const Icon(SolarIconsOutline.videocameraAdd),
                     );
                   }
 
@@ -41,41 +52,104 @@ class MemoryCreateView extends StatelessWidget {
                 Expanded(
                   child: Consumer<MemoryCreateViewmodel>(
                     builder: (_, vm, __) {
-                      return PageView(
-                        controller: vm.pageController,
-                        children: vm.isFromQR
-                            ? []
-                            : [
-                                ...vm.galleryImages.map(
-                                  (e) => Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                      horizontal: 5,
-                                    ),
-                                    child: ExtendedImage.file(
-                                      File(e.path),
-                                    ),
-                                  ),
-                                ),
-                                ...vm.galleryVideos.map(
-                                  (e) => Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                      horizontal: 5,
-                                    ),
-                                    child: VideoPlayer(
-                                      fromNetwork: false,
-                                      uri: null,
-                                      file: e,
-                                    ),
-                                  ),
-                                ),
-                              ],
+                      return Column(
+                        children: [
+                          Expanded(
+                            child: PageView(
+                              controller: vm.pageController,
+                              children: vm.isFromQR
+                                  ? [
+                                      ...vm.crawledImageUrls.map(
+                                        (e) => Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 0,
+                                          ),
+                                          child: Center(
+                                            child: ClipRRect(
+                                              borderRadius: BorderRadius.circular(9),
+                                              child: ExtendedImage.network(
+                                                e,
+                                                fit: BoxFit.fitWidth,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      ...vm.crawledVideoUrls.map(
+                                        (e) => Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 5,
+                                          ),
+                                          child: VideoPlayer(
+                                            fromNetwork: true,
+                                            uri: e,
+                                            file: null,
+                                          ),
+                                        ),
+                                      ),
+                                    ]
+                                  : [
+                                      ...vm.galleryImages.map(
+                                        (e) => Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 0,
+                                          ),
+                                          child: Center(
+                                            child: ClipRRect(
+                                              borderRadius: BorderRadius.circular(9),
+                                              child: ExtendedImage.file(
+                                                File(e.path),
+                                                fit: BoxFit.fitWidth,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      ...vm.galleryVideos.map(
+                                        (e) => Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 5,
+                                          ),
+                                          child: VideoPlayer(
+                                            fromNetwork: false,
+                                            uri: null,
+                                            file: e,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              top: 16,
+                              bottom: 51,
+                            ),
+                            child: SmoothPageIndicator(
+                              controller: vm.pageController,
+                              count: vm.isFromQR
+                                  ? 0
+                                  : vm.galleryImages.length + vm.galleryVideos.length,
+                              effect: const ExpandingDotsEffect(
+                                dotHeight: 6,
+                                dotWidth: 6,
+                                dotColor: ColorFamily.disabledGrey400,
+                              ),
+                            ),
+                          )
+                        ],
                       );
                     },
                   ),
                 ),
                 RoundedButton(
                   onPressed: () => vm.createMemory(context),
-                  child: const Text("픽모리에 저장하기"),
+                  child: const Text(
+                    "픽모리에 저장하기",
+                    style: TextSmStyle(
+                      color: Colors.white,
+                    ),
+                  ),
                 )
               ],
             ),
