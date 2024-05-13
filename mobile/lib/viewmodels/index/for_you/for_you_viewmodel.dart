f1e0d38ef0f08a7633a11fd30d418e5b288a4627
Ksanbal <dev.ksanbal@gmail.com>
2024-05-13 20:50:50 +0900

    Feat: 보관함 레이아웃 적용

--- mobile/lib/viewmodels/index/for_you/for_you_viewmodel.dart
+++ mobile/lib/viewmodels/index/for_you/for_you_viewmodel.dart
@@ -5,10 +5,30 @@ import 'package:picmory/models/album/album_model.dart';
 import 'package:picmory/repositories/album_repository.dart';
 
 class ForYouViewmodel extends ChangeNotifier {
+  ForYouViewmodel() {
+    forYouViewController.addListener(() {
+      final isScrolled = forYouViewController.hasClients && forYouViewController.offset > 0;
+      // 이전 상태와 다를 경우에만 변경
+      if (isShrink != isScrolled) {
+        isShrink = isScrolled;
+        notifyListeners();
+      }
+    });
+  }
+
   final AlbumRepository _albumRepository = AlbumRepository();
 
   final List<AlbumModel> _albums = [];
   List<AlbumModel> get albums => _albums;
+
+  // 추억함 페이지 전체 컨트롤러
+  final ScrollController forYouViewController = ScrollController();
+
+  // 스크롤 최상단 여부 : 앱바 버튼의 스타일을 변경하기 위해서
+  bool isShrink = false;
+
+  // 좋아요 페이지 컨트롤러
+  final PageController likePageController = PageController();
 
   routeToMenu(BuildContext context) {
     context.push('/menu');
--- mobile/lib/views/index/for_you/for_you_view.dart
+++ mobile/lib/views/index/for_you/for_you_view.dart
@@ -1,25 +1,160 @@
 import 'package:flutter/material.dart';
+import 'package:picmory/common/components/page_indicator_widget.dart';
+import 'package:picmory/common/families/color_family.dart';
+import 'package:picmory/common/families/text_styles/caption_sm_style.dart';
+import 'package:picmory/common/families/text_styles/text_sm_style.dart';
 import 'package:picmory/viewmodels/index/for_you/for_you_viewmodel.dart';
 import 'package:provider/provider.dart';
+import 'package:solar_icons/solar_icons.dart';
 
 class ForYouView extends StatelessWidget {
   const ForYouView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final vm = Provider.of<ForYouViewmodel>(context, listen: false);
+    final vm = Provider.of<ForYouViewmodel>(context, listen: true);
 
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () => vm.routeToMenu(context),
-            icon: const Icon(Icons.menu),
+      body: Stack(
+        children: [
+          ListView(
+            controller: vm.forYouViewController,
+            padding: EdgeInsets.only(
+              top: 64 + MediaQuery.of(context).padding.top,
+              bottom: MediaQuery.of(context).padding.bottom + 110,
+            ),
+            children: [
+              // 좋아요
+              SizedBox(
+                height: MediaQuery.of(context).size.width - 32,
+                child: PageView.builder(
+                  itemCount: 6,
+                  controller: vm.likePageController,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: Center(
+                        child: AspectRatio(
+                          aspectRatio: 1 / 1,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: index % 2 == 0 ? Colors.grey : Colors.red,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 16, 16, 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    PageIndicatorWidget(
+                      controller: vm.likePageController,
+                      count: 6,
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: const Row(
+                        children: [
+                          Text(
+                            "좋아요 더보기",
+                            style: TextSmStyle(
+                              color: ColorFamily.textGrey600,
+                            ),
+                          ),
+                          Icon(
+                            SolarIconsOutline.altArrowRight,
+                            color: ColorFamily.textGrey600,
+                            size: 20,
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              // 앨범
+              GridView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 16,
+                  childAspectRatio: 160 / 206,
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 4),
+                        child: AspectRatio(
+                          aspectRatio: 1 / 1,
+                          child: InkWell(
+                            onTap: () {},
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "앨범 이름",
+                        style: TextSmStyle(),
+                      ),
+                      Text(
+                        "123",
+                        style: CaptionSmStyle(
+                          color: ColorFamily.disabledGrey500,
+                        ),
+                      )
+                    ],
+                  );
+                },
+              ),
+            ],
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  CircleAvatar(
+                    backgroundColor:
+                        vm.isShrink ? Colors.black.withOpacity(0.3) : Colors.transparent,
+                    child: IconButton(
+                      icon: const Icon(SolarIconsOutline.addFolder),
+                      color: vm.isShrink ? Colors.white : Colors.black,
+                      onPressed: () {},
+                    ),
+                  ),
+                  const SizedBox(width: 12),
+                  CircleAvatar(
+                    backgroundColor:
+                        vm.isShrink ? Colors.black.withOpacity(0.3) : Colors.transparent,
+                    child: IconButton(
+                      icon: const Icon(SolarIconsOutline.hamburgerMenu),
+                      color: vm.isShrink ? Colors.white : Colors.black,
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
-      ),
-      body: const Center(
-        child: Text("For you"),
       ),
     );
   }
