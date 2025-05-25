b3739eb5fccd86dc376b5c3b4ddcc9ea6bf4a7ed
Ksanbal <dev.ksanbal@gmail.com>
2025-05-25 20:34:23 +0900

    ✨ 앱 리뷰 요청 기능 개발

--- mobile/lib/repositories/internal/in_app_review_repository.dart
+++ mobile/lib/repositories/internal/in_app_review_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:in_app_review/in_app_review.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class InAppReviewRepository {
   final InAppReview inAppReview = InAppReview.instance;
@@ -9,6 +10,9 @@ class InAppReviewRepository {
     try {
       if (await inAppReview.isAvailable()) {
         inAppReview.requestReview();
+      } else {
+        // 스토어 링크로 이동
+        launchUrlString("https://apps.apple.com/kr/app/picmory/id6476240673?action=write-review");
       }
     } catch (e) {
       log('리뷰 요청 중 오류 발생: $e');
--- mobile/lib/viewmodels/app_review_viewmodel.dart
+++ mobile/lib/viewmodels/app_review_viewmodel.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:event_bus/event_bus.dart';
 import 'package:flutter/material.dart';
+import 'package:picmory/common/components/common/modal_comp.dart';
 import 'package:picmory/events/memory/create_event.dart';
 import 'package:picmory/main.dart';
 import 'package:picmory/repositories/internal/in_app_review_repository.dart';
@@ -18,10 +21,10 @@ class AppReviewViewmodel extends ChangeNotifier {
   AppReviewViewmodel({
     required BuildContext context,
   }) : _context = context {
-    print('AppReviewViewmodel initialized');
+    log('AppReviewViewmodel initialized');
+    // resetCounter();
     _loadMemoryCount();
     _subscribeToEvents();
-    // resetCounter();
   }
 
   // 로컬에 저장된 추억 생성 횟수를 가져옵니다.
@@ -29,60 +32,90 @@ class AppReviewViewmodel extends ChangeNotifier {
     final countStr = await _storageRepository.read(StorageKey.memoryCreationCount);
     _memoryCreationCount = countStr != null ? int.parse(countStr) : 0;
 
-    print('_loadMemoryCount: $_memoryCreationCount');
+    log('_loadMemoryCount: $_memoryCreationCount');
   }
 
   // 추억 생성 이벤트 구독
   void _subscribeToEvents() {
-    print('_subscribeToEvents');
+    log('_subscribeToEvents');
     _eventBus.on<MemoryCreateEvent>().listen((_) {
-      print('_subscribeToEvents: MemoryCreateEvent');
+      log('_subscribeToEvents: MemoryCreateEvent');
       _onMemoryCreated();
     });
   }
 
   // 추억 생성 횟수를 증가시키고 3번이 넘으면 리뷰 요청합니다.
   Future<void> _onMemoryCreated() async {
-    print('_onMemoryCreated');
     _memoryCreationCount++;
     await _storageRepository.write(
       StorageKey.memoryCreationCount,
       _memoryCreationCount.toString(),
     );
 
+    log('_onMemoryCreated : $_memoryCreationCount');
+
     if (_memoryCreationCount == _reviewThreshold) {
-      _requestReview();
+      await _requestReview();
     }
   }
 
   // 리뷰 요청
   Future<void> _requestReview() async {
+    log('_requestReview');
+    // 리뷰요청 Event 기록
+    analytics.logEvent(
+      name: 'request_review',
+      parameters: {
+        'status': 'requested',
+      },
+    );
+
+    // 1초 대기
+    await Future.delayed(const Duration(milliseconds: 2000));
+
     final result = await showDialog<bool>(
       context: _context,
-      builder: (context) => AlertDialog(
-        title: const Text('리뷰를 남겨주시겠어요?'),
-        content: const Text('앱이 마음에 드셨다면 리뷰를 남겨주세요!'),
-        actions: [
-          TextButton(
-            child: const Text('나중에'),
-            onPressed: () => Navigator.of(context).pop(false),
-          ),
-          ElevatedButton(
-            child: const Text('리뷰 남기기'),
-            onPressed: () => Navigator.of(context).pop(true),
-          ),
-        ],
+      builder: (context) => ModalComp(
+        title: "벌써 세번이나\n추억을 담아주셨어요!",
+        body: "앱 사용 경험을 공유해주세요.",
+        confirmText: "리뷰할게요",
+        cancelText: "괜찮아요",
+        onConfirm: () => Navigator.of(context).pop(true),
+        onCancel: () => Navigator.of(context).pop(false),
       ),
     );
-    print('result: $result');
+
+    log('_requestReview result: $result');
+
     if (result == true) {
-      // await _inAppReviewRepository.requestReview();
+      await _inAppReviewRepository.requestReview();
+
+      await analytics.logEvent(
+        name: 'request_review',
+        parameters: {
+          'status': 'confirmed',
+        },
+      );
+    } else if (result == false) {
+      await analytics.logEvent(
+        name: 'request_review',
+        parameters: {
+          'status': 'declined',
+        },
+      );
+    } else {
+      await analytics.logEvent(
+        name: 'request_review',
+        parameters: {
+          'status': 'cancelled',
+        },
+      );
     }
   }
 
   // 테스트를 위한 카운터 초기화 메서드
   Future<void> resetCounter() async {
-    _memoryCreationCount = 0;
-    await _storageRepository.write(StorageKey.memoryCreationCount, '0');
+    _memoryCreationCount = 2;
+    await _storageRepository.write(StorageKey.memoryCreationCount, '2');
   }
 }
